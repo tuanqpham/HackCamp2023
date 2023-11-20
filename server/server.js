45e8838e53d378ff5def2744e899d95767f479a0
meomeow <vido@students.cs.ubc.ca>
2023-11-19 18:13:47 -0800

    6.13PM

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+
 const app= express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-})
+app.use(cors());
+app.use(bodyParser.json());
+
+let users = [];
+
+app.post('/signup', (req, res) => {
+    const newUser = req.body;
+    users.push(newUser);
+    saveUsersToFile();
+    res.json({ success: true });
+});
+
+function saveUsersToFile() {
+    const usersData = JSON.stringify(users, null, 2);
+    fs.writeFileSync('users.json', usersData);
+    console.log('Users data saved successfully.');
+}
 
 app.listen(port, () => {
-    console.log('Server running on http://localhost:${port}');
-
+    console.log(`Server is running at http://localhost:${port}`);
 });
